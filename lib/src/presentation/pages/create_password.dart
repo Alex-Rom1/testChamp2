cf600a774c5078bab38110c3fe4c4f6f6dce4bdd
Alex-rom1 <alex.rom495@gmail.com>
2026-01-07 14:59:43 +0300

    added create password page

--- lib/src/presentation/pages/create_password.dart
+++ lib/src/presentation/pages/create_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:matule2/src/presentation/pages/log_in.dart';
 import 'package:test_champ2_ui_kit2/test_champ2_ui_kit2.dart';
 
 class CreatePasswordPage extends StatefulWidget {
@@ -19,7 +21,60 @@ class _CreatePasswordPageState extends State<CreatePasswordPage> {
         child: Center(
           child: Column(
             children: [
-
+              SizedBox(height: 103.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 32.w, height: 32.h),
+                  SizedBox(width: 16.w),
+                  Text('Создание пароля', style: theme.texts.title1Heavy24),
+                ],
+              ),
+              SizedBox(height: 23.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Введите новый пароль',
+                    style: theme.texts.textRegular15,
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+              SizedBox(height: 118.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Новый Пароль',
+                    style: theme.texts.captionRegular14,
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+              SizedBox(height: 4.h),
+              SizedBox(height: 48.h, width: 335.w),
+              SizedBox(height: 12.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Повторите пароль',
+                    style: theme.texts.captionRegular14,
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+              SizedBox(height: 4.h),
+              SizedBox(height: 48.h, width: 335.w),
+              SizedBox(height: 10.h),
+              BigButtonWidget.filled(
+                theme: theme,
+                onTap: () {
+                  Get.to(LogInPage());
+                },
+                text: 'Далее',
+              ),
             ],
           ),
         ),
