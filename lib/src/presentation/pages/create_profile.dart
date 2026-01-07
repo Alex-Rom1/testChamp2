90b7f57954baad5dc01a49fa5c394c1ee430856b
Alex-rom1 <alex.rom495@gmail.com>
2026-01-07 14:00:32 +0300

    added app wrapper

---
+++ lib/src/presentation/pages/create_profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:test_champ2_ui_kit2/test_champ2_ui_kit2.dart';
+
+
+class CreateProfilePage extends StatefulWidget {
+  const CreateProfilePage({super.key});
+
+  @override
+  State<CreateProfilePage> createState() => _CreateProfilePageState();
+}
+
+class _CreateProfilePageState extends State<CreateProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    var theme = CustomTheme.of(context);
+    return Scaffold(
+      body: Center(
+
+      ),
+    );
+  }
+}
